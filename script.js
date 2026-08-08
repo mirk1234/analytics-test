e542a7a6428df6d890485f70bf628d44ccdc2598
mirk1234 <markosadchiy88@gmail.com>
2026-08-08 16:07:56 +0300

    Fix button selector and update event tracking

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const btn = document.querySelector(".bytton");
+const btn = document.querySelector(".event-btn");
 
-btn.addEventListener("click", () => {
-  gtag("event", "user_clicked_on_a_button");
-  console.log("user pressed the buttoning");
-})
+btn?.addEventListener("click", () => {
+  gtag("event", "button_click", {
+    event_category: "interaction",
+    event_label: "user_clicked_on_a_button"
+  });
+  console.log("user pressed the button");
+});
