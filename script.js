f372fd6c1e8ad8a50210a1ae4e5f79d5296b7d40
mirk1234 <markosadchiy88@gmail.com>
2026-08-08 16:11:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector(".event-btn");
 
-btn?.addEventListener("click", () => {
+btn.addEventListener("click", () => {
   gtag("event", "button_click", {
     event_category: "interaction",
     event_label: "user_clicked_on_a_button"
